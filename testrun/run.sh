c29bf6e9990cf76907c60ea03e7a51aa58c765c9
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2023-11-06 13:40:12 -0600

    slim down run script

--- testrun/run.sh
+++ testrun/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # example of a run script for a parallel run on ncores cpu's
-ncores=8
+ncores=1
 
 # number of iterations for the calculation of xgrid at parallelstage 1. It is the old ncall1
 nxgriditeration=3
@@ -49,28 +49,27 @@ prg=../pwhg_main
 
 begin=$(date +"%s")
 
-# parstage=1
-# echo "***********************************************"
-# echo " stage " $parstage
-# echo "***********************************************"
+parstage=1
+echo "***********************************************"
+echo " stage " $parstage
+echo "***********************************************"
 
-# for xgrid in $(seq 1 $nxgriditeration)
-# do
-#     cat powheg.input-save | sed "s/mtdep.*/mtdep $mode/ ; s/parallelstage.*/parallelstage $parstage/ ; s/xgriditeration.*/xgriditeration $xgrid/">powheg.input
-#     cp powheg.input powheg.input-$parstage-$xgrid
-#     logfile=run-$parstage-$xgrid
-#     runthem $mode
-#     wait
-#     rm powheg.input
-# done
-
+for xgrid in $(seq 1 $nxgriditeration)
+do
+    cat powheg.input-save | sed "s/mtdep.*/mtdep $mode/ ; s/parallelstage.*/parallelstage $parstage/ ; s/xgriditeration.*/xgriditeration $xgrid/">powheg.input
+    cp powheg.input powheg.input-$parstage-$xgrid
+    logfile=run-$parstage-$xgrid
+    runthem $mode
+    wait
+    rm powheg.input
+done
 
 parstage=2
 echo "***********************************************"
 echo " stage " $parstage
 echo "***********************************************"
 
-xgrid=3
+xgrid=$nxgriditeration
 
 cat powheg.input-save | sed "s/mtdep.*/mtdep $mode/ ; s/parallelstage.*/parallelstage $parstage/ ; s/xgriditeration.*/xgriditeration $xgrid/">powheg.input
 cp powheg.input powheg.input-$parstage
